a784b1731d1391d6a65579dea9992b05b39a9be4
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-08-08 21:46:57 +0100

    feat : useAuth hook for auth api calls

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,62 @@
+import * as authServices from "../services/auth.service";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+export const useAuth = () => {
+  const meQuery = useQuery({
+    queryKey: ["me"],
+    queryFn: authServices.getMe,
+    retry: false,
+    refetchOnWindowFocus: false,
+  });
+
+  const signupMutation = useMutation({
+    mutationFn: authServices.signup,
+    onSuccess: () => {
+      toast.success("Account created! Check your email to verify.");
+    },
+    onError: () => {
+      toast.error("Signup failed");
+    },
+  });
+
+  const loginMutation = useMutation({
+    mutationFn: authServices.login,
+    onSuccess: () => {
+      toast.success("Login successful");
+    },
+    onError: () => {
+      toast.error("Login failed");
+    },
+  });
+  const logoutMutation = useMutation({
+    mutationFn: authServices.logout,
+  });
+
+  const verifyEmailMutation = useMutation({
+    mutationFn: authServices.verifyEmail,
+  });
+
+  const resendVerificationEmailMutation = useMutation({
+    mutationFn: authServices.resendVerificationEmail,
+  });
+
+  const forgotPasswordMutation = useMutation({
+    mutationFn: authServices.forgotPassword,
+  });
+
+  const resetPasswordMutation = useMutation({
+    mutationFn: authServices.resetPassword,
+  });
+
+  return {
+    signupMutation,
+    loginMutation,
+    logoutMutation,
+    meQuery,
+    verifyEmailMutation,
+    resendVerificationEmailMutation,
+    forgotPasswordMutation,
+    resetPasswordMutation,
+  };
+};
