ef1418a57334a5245d07e719050bf203fc7de06d
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-08-08 21:46:01 +0100

    feat : mount routes and wrap app with toaster and query client provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,17 @@
+import { Auth } from "./pages/Auth";
+import { Home } from "./pages/Home";
+
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/auth" element={<Auth></Auth>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
+        <App />
+      </QueryClientProvider>
+    </Router>
   </StrictMode>,
-)
+);
