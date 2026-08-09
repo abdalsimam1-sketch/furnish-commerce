8c1662ff2e4435d8c879532d75f02c9863feb71c
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-08-09 13:02:38 +0100

    feat : automatic token rotation

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,34 @@
 import axios from "axios";
-
+import { rotateTokens } from "../services/auth.service";
 export const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}`,
   withCredentials: true,
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status !== 401) {
+      return Promise.reject(error);
+    }
+    if (
+      originalRequest.url.includes("/auth/signup") ||
+      originalRequest.url.includes("/auth/login") ||
+      originalRequest.url.includes("/auth/rotate-tokens")
+    ) {
+      return Promise.reject(error);
+    }
+    if (originalRequest._retry) {
+      return Promise.reject(error);
+    }
+    originalRequest._retry = true;
+
+    try {
+      await rotateTokens();
+      return api(originalRequest);
+    } catch (rotateError) {
+      return Promise.reject(rotateError);
+    }
+  },
+);
