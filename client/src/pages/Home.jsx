3dc73f646a9854d17048a3e562194f17d5c98d4c
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-08-14 15:09:27 +0100

    feat : hero section carousel with get categories endpoint

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,62 @@
-import React from "react";
+import { useEffect, useMemo } from "react";
+import { useCategories } from "../hooks/useCategories";
+import { useState } from "react";
 
 export const Home = () => {
-  return <div>Home</div>;
+  const { categoriesQuery } = useCategories();
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const categories = useMemo(
+    () => categoriesQuery.data?.data?.categories ?? [],
+    [categoriesQuery.data],
+  );
+
+  const currentCategory = categories[currentIndex];
+
+  useEffect(() => {
+    if (categories.length === 0) {
+      return;
+    }
+    const timer = setInterval(() => {
+      setCurrentIndex((current) => (current + 1) % categories.length);
+    }, 100000);
+    return () => clearInterval(timer);
+  }, [categories.length]);
+
+  if (categoriesQuery.isLoading)
+    return (
+      <div className="min-vh-100 d-flex justify-content-center align-items-center">
+        <span className="alert alert-secondary">
+          <h3>Loading.....</h3>
+        </span>
+      </div>
+    );
+  if (!currentCategory) return null;
+  return (
+    <div>
+      <section
+        className="hero-section d-flex justify-content-between align-items-center px-2 px-md-5"
+        style={{ backgroundImage: `url(${currentCategory.image})` }}
+      >
+        <i
+          className="bi-chevron-left  btn bg-light "
+          onClick={() =>
+            setCurrentIndex(
+              (current) =>
+                (current - 1 + categories.length) % categories.length,
+            )
+          }
+        ></i>
+        <i
+          className="bi-chevron-right btn bg-light"
+          onClick={() =>
+            setCurrentIndex(
+              (current) =>
+                (current + 1 + categories.length) % categories.length,
+            )
+          }
+        ></i>
+      </section>
+    </div>
+  );
 };
