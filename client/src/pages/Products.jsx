c91156184bd5582ac407edd54f28b3478fd024fa
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-08-18 16:20:23 +0100

    feat : wire add item to cart with login required

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,18 +1,24 @@
 import { useParams } from "react-router-dom";
 import { useProducts } from "../hooks/useProducts";
+import { useCart } from "../hooks/useCart";
+import { LoginRequired } from "../components/LoginRequired";
+import { useEffect, useState } from "react";
 
 export const Products = () => {
   const { categoryId } = useParams();
+  const { addToCartMutation } = useCart();
   const { getCategoryProductsQuery } = useProducts(categoryId);
   const products = getCategoryProductsQuery.data?.data?.products ?? [];
-  console.log(products);
+  const [loginModalOpen, setLoginModalOpen] = useState(false);
+
+  useEffect(() => {
+    document.body.style.overflow = loginModalOpen ? "hidden" : "auto";
+  });
 
   if (getCategoryProductsQuery.isPending) {
     return (
       <div className="min-vh-100 d-flex justify-content-center align-items-center">
-        <span className="alert alert-secondary">
-          <h3>Loading.....</h3>
-        </span>
+        <span className="spinner-border"></span>
       </div>
     );
   }
@@ -42,7 +48,21 @@ export const Products = () => {
                   <span className="fw-bold">
                     ₦{Number(product.price).toLocaleString()}
                   </span>
-                  <button className="btn btn-sm bg-dark text-light">
+                  <button
+                    className="btn btn-sm bg-dark text-light"
+                    onClick={() => {
+                      addToCartMutation.mutate(
+                        { productId: product.id, quantity: 1 },
+                        {
+                          onError: (error) => {
+                            if (error.response.status === 401) {
+                              setLoginModalOpen(true);
+                            }
+                          },
+                        },
+                      );
+                    }}
+                  >
                     Add to Cart
                   </button>
                 </div>
@@ -51,6 +71,9 @@ export const Products = () => {
           </div>
         ))}
       </div>
+      {loginModalOpen && (
+        <LoginRequired onClose={() => setLoginModalOpen(false)}></LoginRequired>
+      )}
     </div>
   );
 };
