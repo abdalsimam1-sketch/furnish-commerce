532342aab7751d378d737acc17285385ed2299a0
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-08-21 10:46:18 +0100

    feat : add create order service

--- server/src/services/orders.service.js
+++ server/src/services/orders.service.js
@@ -31,6 +31,73 @@ const getOrdersService = async (page, limit, search, status) => {
   return { orders, count, totalPages };
 };
 
+const createOrderService = async (userId, checkoutData) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    include: {
+      cart: {
+        include: {
+          cartItems: { include: { product: true } },
+        },
+      },
+    },
+  });
+  const cartItems = user?.cart?.cartItems;
+  const total = cartItems?.reduce(
+    (sum, item) => sum + Number(item?.product?.price) * item?.quantity,
+    0,
+  );
+  const result = await prisma.$transaction(async (tx) => {
+    const order = await tx.order.create({
+      data: {
+        firstName: checkoutData.firstName,
+        lastName: checkoutData.lastName,
+        email: checkoutData.email,
+        streetAddress: checkoutData.streetAddress,
+        country: "Nigeria",
+        phone: checkoutData.phone,
+        city: checkoutData.city,
+        state: checkoutData.state,
+        zipCode: checkoutData.zipCode,
+        customerId: userId,
+        total,
+      },
+    });
+    await Promise.all(
+      cartItems.map((item) =>
+        tx.orderItem.create({
+          data: {
+            quantity: item.quantity,
+            orderId: order.id,
+            price: item.product.price,
+            productId: item.product.id,
+          },
+        }),
+      ),
+    );
+    await Promise.all(
+      cartItems.map((item) =>
+        tx.product.update({
+          where: { id: item.product.id },
+          data: {
+            inStock: item.product.inStock - item.quantity,
+          },
+        }),
+      ),
+    );
+    await tx.cartItem.deleteMany({
+      where: {
+        cartId: user?.cart?.id,
+      },
+    });
+    return order;
+  });
+  return result;
+};
+
 module.exports = {
   getOrdersService,
+  createOrderService,
 };
