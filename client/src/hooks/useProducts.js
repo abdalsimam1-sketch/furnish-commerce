928d282ec2018bc885be247cbd34f633eaa38a6f
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-08-23 20:52:26 +0100

    feat : add get products and get new arrival services and queries

--- client/src/hooks/useProducts.js
+++ client/src/hooks/useProducts.js
@@ -1,10 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 import * as productsServices from "../services/products.service";
+
 export const useProducts = (categoryId) => {
   const getCategoryProductsQuery = useQuery({
     queryKey: ["category products", categoryId],
     queryFn: () => productsServices.getCategoryProducts(categoryId),
     enabled: !!categoryId,
   });
-  return { getCategoryProductsQuery };
+  const getProductsQuery = (page, limit, search, categoryId) =>
+    useQuery({
+      queryKey: ["products", page, limit, search, categoryId],
+      queryFn: () =>
+        productsServices.getProducts(page, limit, search, categoryId),
+      staleTime: 5 * 60 * 1000,
+    });
+  const getNewArrivalsQuery = useQuery({
+    queryKey: ["new arrivals"],
+    queryFn: productsServices.getNewArrivals,
+
+    staleTime: 5 * 60 * 1000,
+  });
+  return { getCategoryProductsQuery, getProductsQuery, getNewArrivalsQuery };
 };
--- client/src/services/products.service.js
+++ client/src/services/products.service.js
@@ -4,3 +4,19 @@ export const getCategoryProducts = async (categoryId) => {
   const response = await api.get(`/products/${categoryId}`);
   return response.data;
 };
+
+export const getProducts = async (page, limit, search, categoryId) => {
+  const response = await api.get("/products", {
+    params: {
+      page,
+      limit,
+      search,
+      categoryId,
+    },
+  });
+  return response.data;
+};
+export const getNewArrivals = async () => {
+  const response = await api.get("/products/new-arrivals");
+  return response.data;
+};
