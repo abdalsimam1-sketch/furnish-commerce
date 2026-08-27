35297dfda8787d2f2daebfc0172dfea57da06b14
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-08-27 11:28:52 +0100

    feat : products crud routes

--- server/src/routes/products.route.js
+++ server/src/routes/products.route.js
@@ -1,8 +1,28 @@
 const productsRouter = require("express").Router();
 const productsController = require("../controllers/products.controller");
+const { authentication } = require("../middleware/authentication");
+const { authorization } = require("../middleware/authorization");
 
 productsRouter.get("/", productsController.getProducts);
+productsRouter.post(
+  "/add-product",
+  authentication,
+  authorization("admin"),
+  productsController.addProduct,
+);
 productsRouter.get("/new-arrivals", productsController.getNewArrivals);
+productsRouter.patch(
+  "/edit-product/:productId",
+  authentication,
+  authorization("admin"),
+  productsController.editProduct,
+);
+productsRouter.delete(
+  "/delete-product/:productId",
+  authentication,
+  authorization("admin"),
+  productsController.deleteProduct,
+);
 productsRouter.get("/:categoryId", productsController.getCategoryProducts);
 
 module.exports = {
