4d4318da8976f5c9a9ae5d2188ad42b395989b3a
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-08-29 11:09:06 +0100

    feat : make image not required and handled that validation manually in the controller

--- server/src/validations/products.validation.js
+++ server/src/validations/products.validation.js
@@ -4,7 +4,6 @@ const addProductSchema = Joi.object({
   name: Joi.string().required(),
   inStock: Joi.number().min(0).required(),
   description: Joi.string().required(),
-  image: Joi.string().uri().required(),
   price: Joi.number().positive().precision(2).required(),
   categoryId: Joi.string().required(),
 });
