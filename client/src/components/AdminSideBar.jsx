1ea37ad691ae939fded82afca1ed4ccdd45a63b1
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-08-31 16:34:33 +0100

    refactor : reduce button sizes

--- client/src/components/AdminSideBar.jsx
+++ client/src/components/AdminSideBar.jsx
@@ -8,38 +8,38 @@ export const AdminSideBar = ({ onLogoutClick }) => {
   };
   return (
     <div className="border-end  h-100  m-0 admin-side-bar text-center pt-4 d-flex flex-column gap-5">
-      <h1 className="">Furnish</h1>
+      <h1>Furnish</h1>
       <div
         style={{ height: "80vh" }}
         className="d-flex flex-column justify-content-between px-5 "
       >
         <div className="d-flex flex-column gap-4 text-start">
           <Link
-            className={`btn text-start  ${activeLink("/admin/dashboard")}`}
+            className={`btn text-start btn-sm ${activeLink("/admin/dashboard")}`}
             to="/admin/dashboard"
           >
             <i className="bi bi-grid me-3"></i>
             Dashboard
           </Link>
           <Link
-            className={`btn text-start  ${activeLink("/admin/orders")}`}
+            className={`btn text-start btn-sm  ${activeLink("/admin/orders")}`}
             to="/admin/orders"
           >
             <i className="bi-receipt me-3"></i>Orders
           </Link>
           <Link
-            className={`btn text-start  ${activeLink("/admin/products")}`}
+            className={`btn text-start btn-sm ${activeLink("/admin/products")}`}
             to="/admin/products"
           >
             <i className="bi-box-seam me-3"></i>Products
           </Link>
           <Link
-            className={`btn text-start  ${activeLink("/admin/users")}`}
+            className={`btn text-start btn-sm ${activeLink("/admin/users")}`}
             to="/admin/users"
           >
             <i className="bi-people me-3"> </i>Users
           </Link>
-          <Link className="btn bg-dark text-start text-light" to="/">
+          <Link className="btn bg-dark btn-sm text-start text-light" to="/">
             <i className="bi-house me-3"> </i>Back to Store
           </Link>
         </div>
