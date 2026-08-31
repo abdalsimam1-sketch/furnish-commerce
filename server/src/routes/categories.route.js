5e297a35c6c40f346c1a4fca93d5c0bde87ba453
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-08-31 11:15:21 +0100

    feat : add new category endpoint

--- server/src/routes/categories.route.js
+++ server/src/routes/categories.route.js
@@ -1,7 +1,17 @@
 const categoriesRouter = require("express").Router();
-const { getCategories } = require("../controllers/categories.controller");
+const categoriesController = require("../controllers/categories.controller");
+const { authentication } = require("../middleware/authentication");
+const { authorization } = require("../middleware/authorization");
+const { fileUpload } = require("../middleware/fileUpload");
 
-categoriesRouter.get("/", getCategories);
+categoriesRouter.get("/", categoriesController.getCategories);
+categoriesRouter.post(
+  "/",
+  authentication,
+  authorization("admin"),
+  fileUpload.single("categoryImage"),
+  categoriesController.addNewCategory,
+);
 
 module.exports = {
   categoriesRouter,
